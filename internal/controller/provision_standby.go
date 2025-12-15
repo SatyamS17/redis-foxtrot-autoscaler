80e08e5e5bb1ec19bf4b5404ed85c008c93dbfee
satyams2 <satyams2@illinois.edu>
2025-12-14 19:56:19 -0600

    gofmt: format provision_standby controller

--- internal/controller/provision_standby.go
+++ internal/controller/provision_standby.go
@@ -7,8 +7,8 @@ import (
 
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
